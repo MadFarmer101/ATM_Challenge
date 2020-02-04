500b51575adc82b63421b63e1423e041ab459317
Janko <mailodjanka@gmail.com>
2020-02-04 12:44:19 +0100

    Refactored the withdraw method and added two new methods

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,20 +1,31 @@
 class Atm
+  
   attr_accessor :funds
   require 'date'
-  
+
   def initialize
     @funds = 1000
   end
 
   def withdraw(amount, account)
     case 
-    when amount > account.balance
+    when insufficient_funds_in_account?(amount, account)
       return
     else
-      @funds -=amount
-      account.balance = account.balance - amount
-      { status: true, message: 'success', date: Date.today, amount: amount }
-    end
+      perform_transaction(amount, account)
+  end
+end
+
+private
+
+  def insufficient_funds_in_account?(amount, account)
+    amount > account.balance
+  end
+
+  def perform_transaction(amount, account)
+    @funds -= amount
+    account.balance = account.balance - amount
+    { status: true, message: 'success', date: Date.today, amount: amount}
   end
 end
 

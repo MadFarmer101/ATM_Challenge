1d49889fce1dcdadc0bc0c633c96c7eb4c2ce3f2
Jaime <jaime.cruz.hostalot@gmail.com>
2020-02-04 12:52:37 +0100

    Added sad path, user cannot extract more money than he has

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -7,6 +7,11 @@ describe Atm do
     allow(account).to receive(:balance=)
   end
 
+  it 'rejects withdraw if account has insufficient funds' do
+    expected_output = {status: false, message: 'insufficient funds', date: Date.today}
+    expect(subject.withdraw(105, account)).to eq expected_output
+  end
+
   it 'allow withdraw if the account has enough balance.' do
     expected_output = { status: true, message: 'success', date: Date.today, amount: 45}
     expect(subject.withdraw(45, account)).to eq expected_output

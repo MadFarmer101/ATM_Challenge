feaf4fc632f67246aae9ea5bddf2ba34ebb3f4be
Blake <blake@futchi.com>
2020-02-06 10:25:41 +0100

    issues with connecting person class unexpected end of input

--- lib/person.rb
+++ lib/person.rb
@@ -8,5 +8,13 @@ class Person
         @name = :name 
     end
 
-    
 end
+
+private
+
+def name(obj)
+  obj == nil ?  missing_name : @name = obj
+end
+
+def missing_name
+  raise "A name is required"

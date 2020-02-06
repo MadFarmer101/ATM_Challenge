4c2f83b8ac7636e8456be73b568a15ee4ce92c13
Blake <blake@futchi.com>
2020-02-06 16:09:18 +0100

    can manage funds with account

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -43,7 +43,15 @@ describe Person do
         it 'can deposit funds' do
             expect(subject.deposit(100)).to be_truthy
         end
+
+        it 'funds are added to the account balance - deducted from cash' do
+          subject.cash = 100
+          subject.deposit(100)
+          expect(subject.account.balance).to be 100
+          expect(subject.cash).to be 0
+        end
     end
+
     describe 'can not manage funds if no account has been created do' do
       it 'can\'t deposit funds' do
         expect { subject.is_there_account?(100)}.to raise_error(RuntimeError, 'No account present')
